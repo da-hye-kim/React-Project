7ac2efca71066bf5e85977a8abd45ab042eb0883
da-hye-kim <weraaa1992@naver.com>
2024-01-09 10:10:24 +0900

    📝 로그인 페이지 생성

---
+++ LoginPage.js
@@ -0,0 +1,13 @@
+import './LoginPage.css';
+
+function LoginPage() {
+    return (
+        <div>
+            <form>
+                <label>Email</label>
+            </form>
+        </div>
+    )
+}
+
+export default LoginPage;
--- react-app/src/App.js
+++ react-app/src/App.js
@@ -2,9 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  //debugger = 내가원하는 지점에서 debugger를 쓰면 자바 디버깅 처럼 값이 잘들어오는지 확인할 수 있다.
   return (
     <div className="App">
       <header className="App-header">
+        <a className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="loginPage"><h1>시작 페이지</h1></a>
+      </header>
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -20,7 +27,7 @@ function App() {
         <h1>React Start</h1>
         <p>fuhjysdfjshdfjksdfhsdjkfhsdjkhfjksdhfjksdhfhsd</p>
         <button>누르시오!!!!!!!aaaaaa</button>
-      </header>
+      </header> */}
     </div>
   );
 }
